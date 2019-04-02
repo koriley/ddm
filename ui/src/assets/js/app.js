79698531390f4e80e0ffcecc0f9b1f01076dd075
Scott Sauer <ssauer@basspro.com>
2019-04-02 14:51:10 -0500

    style stuff

--- ui/src/assets/js/app.js
+++ ui/src/assets/js/app.js
@@ -1,30 +1,8 @@
-jQuery("document").ready(function () {
-    jQuery(".ability").each(function () {
-        jQuery(this).on("focus", function () {
-            jQuery(this).select();
-        })
-        $(this).on("keyup", function () {
-            var ability = $(this).val();
-            var modifier = $(this).closest('.ability-box').children('.badge').text();
-            if (!$.isNumeric(ability)) {
-                ability = 1;
-                $(this).val(ability);
-            }
-            //     if (ability < 1) {
-            //         ability = 1;
-            //         $(this).val(ability);
-            //     }
-            //     if (ability > 30) {
-            //         ability = 30;
-            //         $(this).val(ability);
-            //     }
+jQuery("document").ready(function() {
 
-            modifier = Math.floor((ability - 10) / 2)
-            $(this).closest('.ability-box').children('.badge').text(modifier);
-        });
-    });
-
-    jQuery(function () {
+    jQuery(function() {
         jQuery(".drag").draggable();
     });
+
+
 })
---
+++ ui/src/assets/js/styler.js
@@ -0,0 +1,10 @@
+//styler
+var buttonOptions = ['default', 'black', 'gray', 'light', 'red'];
+jQuery(buttonOptions).each(function() {
+    var myStyle = this;
+    var myId = '#' + myStyle;
+    jQuery(myId).on("click", function() {
+        $("body").removeAttr('class');
+        jQuery('body').addClass('theme-' + myStyle);
+    })
+})
