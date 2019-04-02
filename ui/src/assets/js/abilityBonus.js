e987dda8d3fa70ea9081d64d6bdc999f2b2ea7e9
Kevin O'Riley <kevin@blueshorestudios.com>
2019-04-02 14:46:42 -0500

    one js to rule them all.

---
+++ ui/src/assets/js/abilityBonus.js
@@ -0,0 +1,24 @@
+jQuery(".ability").each(function () {
+    jQuery(this).on("focus", function () {
+        jQuery(this).select();
+    })
+    $(this).on("keyup", function () {
+        var ability = $(this).val();
+        var modifier = $(this).closest('.ability-box').children('.badge').text();
+        if (!$.isNumeric(ability)) {
+            ability = 1;
+            $(this).val(ability);
+        }
+        //     if (ability < 1) {
+        //         ability = 1;
+        //         $(this).val(ability);
+        //     }
+        //     if (ability > 30) {
+        //         ability = 30;
+        //         $(this).val(ability);
+        //     }
+
+        modifier = Math.floor((ability - 10) / 2)
+        $(this).closest('.ability-box').children('.badge').text(modifier);
+    });
+});
