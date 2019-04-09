5f5a31595d21f7dfc2d0e45c0b013cdb2bf2103f
Kevin O'Riley <kevin@blueshorestudios.com>
2019-04-09 09:54:14 -0500

    Added the ability to read books.

--- ui/src/assets/js/app.js
+++ ui/src/assets/js/app.js
@@ -1,6 +1,18 @@
-jQuery("document").ready(function() {
+'use strict';
+//remove line below when testing frontend
+var fs = require('fs');
+//////////////////
+
+jQuery("document").ready(function () {
     jQuery(".drag").draggable({
         containment: ".mainView",
         scroll: false
     });
+});
+
+var books = {};
+readAFile("./books/dmg.json").then((book) => {
+    var bookTitle = JSON.parse(book);
+
+    console.log(bookTitle.details.name);
 });
--- ui/src/assets/js/io.js
+++ ui/src/assets/js/io.js
@@ -1,28 +1,28 @@
-// var fs = require('fs');
-//
-// var readAFile = (filepath) => {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(filepath, 'utf-8', function(err, data) {
-//             if (err) {
-//                 reject("An error ocurred reading the file :" + err.message);
-//                 return;
-//             } else {
-//                 resolve(data);
-//             }
-//         });
-//     });
-// }
-//
-// var writeFile = (filepath, data) => {
-//     return new Promise((resolve, reject) => {
-//       fs.writeFile(filepath, data, (err) => {
-//           if (err) {
-//               reject(err);
-//           }
-//       });
-//     })
-// }
-//
-// //write test
+'use strict';
+
+function readAFile(filepath){
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, 'utf-8', function(err, data) {
+            if (err) {
+                reject("An error ocurred reading the file :" + err.message);
+                return;
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function writeFile(filepath, data) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filepath, data, (err) => {
+          if (err) {
+              reject(err);
+          }
+      });
+    })
+}
+
+//write test
 // var testObj  = "This is a write test";
 // writeFile("../test.txt", testObj);
